29bf1bfdcd17fab6def789b6483723ef2ca27b35
jasonlpapadopoulos <87910990+jasonlpapadopoulos@users.noreply.github.com>
2025-02-01 09:10:45 -0500

    logs and icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     <HelmetProvider>
       <Head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        {/* <link rel="icon" href="/favicon.svg" type="image/svg+xml" /> */}
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Header />
       <Component {...pageProps} />
