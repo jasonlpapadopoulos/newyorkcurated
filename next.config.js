c5b0a989e1ca8054f9502c7e0297bd838079a65e
jasonlpapadopoulos <87910990+jasonlpapadopoulos@users.noreply.github.com>
2025-02-01 10:39:49 -0500

    remove unsafe eval

--- next.config.js
+++ next.config.js
@@ -2,41 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:",
-              "script-src * 'unsafe-inline' 'unsafe-eval'",
-              "style-src * 'unsafe-inline'",
-              "img-src * data: blob:",
-              "font-src * data:",
-              "connect-src *",
-              "media-src *"
-            ].join('; ')
-          }
-        ]
-      }
-    ];
-  }
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.devtool = 'source-map'; // Avoids using eval() in production
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   webpack: (config, { dev }) => {
-//     if (!dev) {
-//       config.devtool = 'source-map'; // Avoids using eval() in production
-//     }
-//     return config;
-//   },
-// };
-
-// module.exports = nextConfig;
