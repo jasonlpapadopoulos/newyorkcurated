6a6dff9cb1d3def1c6f3ea63fcf11e7b663b0e0e
jasonlpapadopoulos <87910990+jasonlpapadopoulos@users.noreply.github.com>
2025-02-03 17:26:58 -0500

    g

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true, // Ensures consistent URLs
+  output: "standalone", // Forces SSR pages to be treated as standalone functions
+  experimental: {
+    appDir: true,
+  },
   webpack: (config, { dev }) => {
       if (!dev) config.devtool = false;
       config.optimization = { ...config.optimization, minimize: true };
