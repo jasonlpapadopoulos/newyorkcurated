a3f8100f824804ff9da3ec6f24c3c8b2ab7a2078
jasonlpapadopoulos <87910990+jasonlpapadopoulos@users.noreply.github.com>
2025-02-10 22:07:57 -0500

    api file

---
+++ src/pages/api/neighborhoods.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { query } from '../../lib/db';
+import type { Neighborhood } from '../../types/neighborhood';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const result = await query('SELECT neighborhood_name, neighborhood_name_clean, description FROM neighborhoods');
+
+    const neighborhoods: Neighborhood[] = (result as any[]).map(neighborhood => ({
+      name: neighborhood.neighborhood_name,
+      value: neighborhood.neighborhood_name_clean,
+      description: neighborhood.description,
+      image: `https://source.unsplash.com/1600x900/?nyc,${neighborhood.neighborhood_name_cleaned}` // Default image link, adjust if needed
+    }));
+
+    res.status(200).json(neighborhoods);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
